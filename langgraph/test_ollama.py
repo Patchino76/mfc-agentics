fb244b4edd2381a6159dd846709eee1d061957bd
Svetoslav Lyubenov <patchino76@gmail.com>
2025-01-15 18:28:51 +0200

    1

--- langgraph/test_ollama.py
+++ langgraph/test_ollama.py
@@ -4,7 +4,8 @@ import ollama
 # %%
 try:
     # Replace "llama3.1:latest" with the actual model name
-    test_response = ollama.chat("llama3.1:latest", "Hello, test prompt.")
+    test_response = ollama.chat(model = "granite3.1-dense:8b", 
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],)
     print("Connection successful:", test_response)
 except Exception as e:
     print("Connection error:", str(e))
